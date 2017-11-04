9425d77113257e3180607557d385e460d58d9573
ionajewel <ipjwilli@gmail.com>
2017-11-03 22:30:49 -0700

    remove config.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const models = require('../models');
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
